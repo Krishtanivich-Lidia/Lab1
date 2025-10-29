7e082e4e9f358478b1248b3de5451a9ac8ca620d
Krishtanivich-Lidia <kryshtanovych.lidiia@chnu.edu.ua>
2025-10-29 17:04:39 +0200

    Add project files.

---
+++ ConsoleApp1/SixTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SixTask
+    {
+        public static void Sixtask()
+        {
+            Console.Write("Enter an integer:");
+            int number=int.Parse(Console.ReadLine());
+
+            if (number %2==0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ThirdTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ThirdTask1
+    {
+        public static void Thirdtask()
+        {
+            Console.Write("Enter hours(0-23):");
+            int h=int.Parse(Console.ReadLine());
+
+            if (h>=5 && h<12)
+            {
+                Console.WriteLine("Good day!");
+            }
+            else if (h>=12 && h<18)
+            {
+                Console.WriteLine("Good day!");
+            }
+            else if (h>=18 && h<22)
+            {
+                Console.WriteLine("Good night!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+            }
+        }
+    }
+}
